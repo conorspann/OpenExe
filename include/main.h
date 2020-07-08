b02946cc02eed856b79f4ddea864bb03829e5608
Conor Spann <conorspann@gmail.com>
2020-07-08 13:07:06 +0100

    OE4 Determine OS

--- include/main.h
+++ include/main.h
@@ -2,6 +2,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int openexe_execute(std::string pathToExe, std:vector<std::string> cmdArgs)
+#if defined(_WIN32) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__)
+    #define OS_NAME "windows"
+#elif defined(__unix) || defined(__unix__)
+    #define OS_NAME "unix"
+#elif defined(__APPLE__) || defined(__MACH__)
+    #define OS_NAME "mac"
+#elif defined(__linux__)
+    #define OS_NAME "linux"
+#else
+    #define OS_NAME "unknown"
+#endif
+
+int openexe_execute(std::string pathToExe, std::vector<std::string> cmdArgs);
 
 #endif // MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "../include/main.h"
 
 
-int openexe_execute(std::string pathToExe, std:vector<std::string> cmdArgs)
+int openexe_execute(std::string pathToExe, std::vector<std::string> cmdArgs)
 {
+    std::cout << OS_NAME << std::endl;
 
     return 0;
 }
-
 
