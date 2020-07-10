e4d8217b37e11b11354a5a0d7e52f2d6c232b461
Conor Spann <conorspann@gmail.com>
2020-07-10 13:30:13 +0100

    OE6 Windows Include

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,15 @@
 
 #include "../include/main.h"
 
+#if OPENEXE_OS_NAME == OPENEXE_OS_WINDOWS
+    #include "windows.h"
+#endif
+
 int openexe::execute(std::string pathToExe, std::vector<std::string> cmdArgs)
 {
+    #if OPENEXE_OS_NAME == OPENEXE_OS_WINDOWS
+        //CreateProcessA(pathToExe.c_str(), cmdArgs, NULL, NULL, false, 0, NULL, NULL, );
+    #endif
 
     return 0;
 }
